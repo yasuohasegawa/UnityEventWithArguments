dd62aaa2377d6f4c52c434ce2954d9499e9510e8
HasegawaYasuo <yasuointeractive@gmail.com>
2018-04-21 17:50:38 +0900

    Initial commit.

---
+++ Assets/Scripts/App.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class App : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string eventName = "TEST_EVENT";
+        EventManager.StartListening(eventName, OnTest);
+        EventManager.TriggerEvent(eventName, "aaa", 1234);
+        EventManager.StopListening(eventName, OnTest);
+        EventManager.TriggerEvent(eventName);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTest(object[] p){
+        Debug.Log(p[0]+","+p[1]);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # UnityEventWithArguments
 You can pass arguments with UnityEvent.
+Code is based from "https://unity3d.com/jp/learn/tutorials/topics/scripting/events-creating-simple-messaging-system".<br>
+<br>
+## Example Code
+```C#
+string eventName = "TEST_EVENT";
+EventManager.StartListening(eventName, OnTest);
+EventManager.TriggerEvent(eventName, "aaa", 1234);
+EventManager.StopListening(eventName, OnTest);
+EventManager.TriggerEvent(eventName);
+
+public void OnTest(object[] p){
+    Debug.Log(p[0]+","+p[1]);
+}       
+```
